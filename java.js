9504322a94fc98401c6411c55c079c98a8d95acd
Xaruneo <115806811+Xaruneo@users.noreply.github.com>
2022-10-15 22:30:42 -0500

    Update java.js

--- java.js
+++ java.js
@@ -1,8 +1,25 @@
-const iconMenu = document.getElementById("icon-menu");
-const mainNav = document.getElementById("main-nav");
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
 
-iconMenu.addEventListener("click", () => {
-  mainNav.classList.toggle("show-nav");
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
 
-  iconMenu.classList.toggle("close-menu");
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
 });
